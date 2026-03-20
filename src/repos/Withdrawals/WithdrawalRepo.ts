a190e3d55e7d3f54c33cfb96c95cc839daa545ed
Miguel Cardoso <miguel.cardoso@onlyhighiq.com>
2026-03-20 15:20:00 +0000

    Merged in create-withdrawals-table (pull request #29)
    Withdrawals Table
    
    * Withdrawals Table

--- src/repos/Withdrawals/WithdrawalRepo.ts
+++ src/repos/Withdrawals/WithdrawalRepo.ts
@@ -7,11 +7,11 @@ import Logger from "../../loaders/logger";
 
 @Service()
 export default class WithdrawalRepo implements IWithdrawalRepo {
-    private table = `"Withdrawal"`;
+  private table = `"Withdrawals"`;
 
-    public async getAllWithdrawals(accountId: string, page: number): Promise<Withdrawal[]> {
-        const offset = (page - 1) * 50;
-        const query = `
+  public async getAllWithdrawals(accountId: string, page: number): Promise<Withdrawal[]> {
+    const offset = (page - 1) * 50;
+    const query = `
         SELECT w.*
         FROM ${this.table} w
         JOIN "Wallet" wa ON wa."walletAddress" = w."walletAddress"
@@ -26,18 +26,17 @@ export default class WithdrawalRepo implements IWithdrawalRepo {
     Logger.info({ accountId }, "Retrieved All Withdrawals.");
 
     return result.rows.map(row => WithdrawalMap.fromPersistence(row));
-    }
+  }
 
-    public async getWithdrawalById(withdrawalId: string): Promise<Withdrawal | null> {
-        throw new Error("Method not implemented.");
-    }
+  public async getWithdrawalById(withdrawalId: string): Promise<Withdrawal | null> {
+    throw new Error("Method not implemented.");
+  }
 
-    public async createWithdrawal(withdrawal: Withdrawal): Promise<Withdrawal> {
-        throw new Error("Method not implemented.");
-    }
+  public async createWithdrawal(withdrawal: Withdrawal): Promise<Withdrawal> {
+    throw new Error("Method not implemented.");
+  }
 
-    public async updateWithdrawalStatus(withdrawalId: string, status: string): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
+  public async updateWithdrawalStatus(withdrawalId: string, status: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
 }
-    
